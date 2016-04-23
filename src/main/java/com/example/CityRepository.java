b8a51fde9cbb3e58a4f93a5fc4d72e0b73b27b4f
qinhang.qh <qinhang.qh@alibaba-inc.com>
2016-04-24 01:49:48 +0800

    spring boot demo

--- src/main/java/com/example/CityRepository.java
+++ src/main/java/com/example/CityRepository.java
@@ -10,6 +10,10 @@ public interface CityRepository extends Repository<City, Long> {
 
     Page<City> findAll(Pageable pageable);
 
+    int count();
+
+//    Page<City> findByNameAll(Pageable pageable,String name);
+
     City findByNameAndStateAllIgnoringCase(String name, String state);
 }
 
--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -1,28 +1,26 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
     @Autowired
-    private CityRepository cityRepository;
+    private NewCityRepository newCityRepository;
 
     @RequestMapping("/")
     @ResponseBody
     public String home() {
-        return "Hello World!";
+        return "Hello World!!!!";
     }
 
     @RequestMapping(value="/{user}", method=RequestMethod.GET)
@@ -34,16 +32,20 @@ public class SampleController {
     @RequestMapping(value="/city", method=RequestMethod.GET)
     @ResponseBody
     public String getCity() {
-        Pageable pageable = new PageRequest(1,10);
-        Page<City> cities = cityRepository.findAll(pageable);
+//        Pageable pageable = new PageRequest(1,10);
+        List<City> cities = newCityRepository.findAll();
         StringBuilder stringBuilder = new StringBuilder();
-        for (City city : cities) {
-            stringBuilder.append(city);
-        }
+        cities.forEach(stringBuilder::append);
         return stringBuilder.toString();
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
+    @RequestMapping(value="/countCity", method=RequestMethod.GET)
+    @ResponseBody
+    public String getCountCity() {
+        return String.valueOf(newCityRepository.count());
     }
+
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(SampleController.class, args);
+//    }
 }
