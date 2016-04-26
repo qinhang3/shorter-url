dfc89ad58c17247050e5ac6adc91d41e5175fff6
qinhang.qh <qinhang.qh@alibaba-inc.com>
2016-04-26 21:04:34 +0800

    spring boot demo

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -1,49 +1,42 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-    @Autowired
-    private NewCityRepository newCityRepository;
-
-    @RequestMapping("/")
-    @ResponseBody
-    public String home() {
-        return "Hello World!!!!";
-    }
-
-    @RequestMapping(value="/{user}", method=RequestMethod.GET)
-    @ResponseBody
-    public String getUser(@PathVariable String user) {
-        return user;
-    }
-
-    @RequestMapping(value="/city", method=RequestMethod.GET)
-    @ResponseBody
-    public String getCity() {
-//        Pageable pageable = new PageRequest(1,10);
-        List<City> cities = newCityRepository.findAll();
-        StringBuilder stringBuilder = new StringBuilder();
-        cities.forEach(stringBuilder::append);
-        return stringBuilder.toString();
-    }
-
-    @RequestMapping(value="/countCity", method=RequestMethod.GET)
-    @ResponseBody
-    public String getCountCity() {
-        return String.valueOf(newCityRepository.count());
-    }
+//    @Autowired
+//    private NewCityRepository newCityRepository;
+//
+//    @RequestMapping("/")
+//    @ResponseBody
+//    public String home() {
+//        return "Hello World!!!!";
+//    }
+//
+//    @RequestMapping(value="/{user}", method=RequestMethod.GET)
+//    @ResponseBody
+//    public String getUser(@PathVariable String user) {
+//        return user;
+//    }
+//
+//    @RequestMapping(value="/city", method=RequestMethod.GET)
+//    @ResponseBody
+//    public String getCity() {
+////        Pageable pageable = new PageRequest(1,10);
+//        List<City> cities = newCityRepository.findAll();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        cities.forEach(stringBuilder::append);
+//        return stringBuilder.toString();
+//    }
+//
+//    @RequestMapping(value="/countCity", method=RequestMethod.GET)
+//    @ResponseBody
+//    public String getCountCity() {
+//        return String.valueOf(newCityRepository.count());
+//    }
 
 //    public static void main(String[] args) throws Exception {
 //        SpringApplication.run(SampleController.class, args);
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example;
 
+import com.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class DemoApplicationTests {
 
 	@Test
