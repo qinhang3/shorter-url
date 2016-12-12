9b91cfe80a582944a860cb338467bcd5d45d1e03
qinhang3 <qinhang3@hotmail.com>
2016-12-13 00:59:17 +0800

    react初始化

---
+++ src/main/resources/js/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!!<br />
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/main/resources/webpack.config.js
@@ -0,0 +1,23 @@
+var config = {
+    entry: './js/main.js',
+    output: {
+        path:'./static',
+        filename: 'index.js',
+    },
+    devServer: {
+        inline: true,
+        port: 7777
+    },
+    module: {
+        loaders: [ {
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
+
+module.exports = config;
